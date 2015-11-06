d07bfec3b7b7cf65e561801fc64adc58f27ed2e6
root <root@localhost.localdomain>
2015-11-06 17:47:13 +0900

    テスト

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCUTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Quiz</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div id="container">
+	 <h1>Q. What is A?</h1>
+	 <ul>
+	     <li class="answer">A0</li>
+	     <li class="answer">A1</li>
+	     <li class="answer">A2</li>
+	     <li class="answer">A3</li>
+	 </ul>
+	</div>
+	<div id="btn" class="disabled">Next Question</div>
+	</div>
+</body>
+</html>
+
